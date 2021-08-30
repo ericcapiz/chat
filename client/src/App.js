7141dea27ca2c7b582db8933f400d3f1116997dc
ericcapiz <ericcapiz@gmail.com>
2021-08-30 13:58:14 -0500

    channel list container started

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer } from './components';
 import './App.css';
+
+const apiKey = '7pxmuau777mq';
+
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client ={client} theme="team light">
+          <ChannelListContainer />
+
+          <ChannelContainer />
+      </Chat>
     </div>
   );
 }
---
+++ client/src/components/ChannelContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelContainer = () => {
+    return (
+        <div>
+            cont
+        </div>
+    )
+}
+
+export default ChannelContainer
