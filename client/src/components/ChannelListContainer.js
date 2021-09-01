73a0e7b48f37472d480c7f93c01d2b57f951db93
ericcapiz <ericcapiz@gmail.com>
2021-09-01 13:24:18 -0500

    users can search for users and channels

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -58,7 +58,7 @@ const ChannelListContent = ({isCreating, setIsCreating, setIsEditing, setCreateT
             <SideBar logout={logout} />
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
-                <ChannelSearch />
+                <ChannelSearch setToggleContainer={setToggleContainer} />
                 <ChannelList 
                     filters={filters} 
                     channelRenderFilterFn={customChannelTeamFilter} 
